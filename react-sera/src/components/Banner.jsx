cda8daffc1e9eed334e6d44ae7788f1bca3a763a
Renzobor <renzo-ejem@hotmail.com>
2024-08-09 20:15:10 -0300

    Mejora banner

--- react-sera/src/components/Banner.jsx
+++ react-sera/src/components/Banner.jsx
@@ -1,9 +1,16 @@
 import banner from "../assets/fondo.png"
 
-function Banner(){
-    return(
+function Banner() {
+    return (
         <section id="banner-container">
             <img id="banner" src={banner} alt="Banner principal" />
+            <section id="text-container">
+                <span className="text-banner">SOMOS FABRICANTES</span>
+                <span className="text-banner">INDUMENTARIA PARA EMPRESAS</span>
+                <span className="text-banner">SEGURIDAD INDUSTRIAL</span>
+                <span className="text-banner">MERCHANDISING</span>
+                <button>Productos</button>
+            </section>
         </section>
     )
 }
